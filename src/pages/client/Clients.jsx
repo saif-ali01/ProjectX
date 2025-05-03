e532dd493cb109a03ee5d78c83a14fcfe9a39626
saif-ali01 <saif-ali01@outlook.com>
2025-05-04 01:01:37 +0530

    Fix: Add @vitejs/plugin-react and config

--- src/pages/client/Clients.jsx
+++ src/pages/client/Clients.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Component } from "react";
 import { Users, Search, Plus, Trash2 } from "lucide-react";
 import Toast from "../../components/common/Toast";
 import { api } from "../../utils/api";
